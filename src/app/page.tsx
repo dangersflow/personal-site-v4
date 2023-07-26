99b4c8279ec856193a59157beaa65470a08a3684
Frank <pacog879@hotmail.com>
2023-07-26 00:13:29 -0500

    update: added background image && made name animation look like typing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,181 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import {
+  AnimatePresence,
+  animate,
+  motion,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
+import localFont from "next/font/local";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import image from "../../public/abstract.jpg";
+
+// Font files can be colocated inside of `pages`
+const myFont = localFont({ src: "../../public/Canno.ttf" });
+const myFont2 = localFont({ src: "../../public/Galgo.otf" });
+
+const firstName = "francisco";
+const lastName = "gonzalez.";
+
+const MainWrapper = styled(motion.main)`
+  height: 100dvh;
+  overflow-y: scroll;
+  scroll-snap-type: y mandatory;
+`;
+
+const Section = styled(motion.section)`
+  display: flex;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 13rem;
+  text-align: center;
+  text-wrap: balance;
+  scroll-snap-align: start;
+  scroll-snap-stop: always;
+  height: 100dvh;
+
+  background-color: #dfdbe5;
+`;
+
+const Text = styled(motion.span)`
+  font-size: 5rem;
+`;
 
 export default function Home() {
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+
+  useEffect(() => {
+    const controls = animate(count, 100);
+
+    return controls.stop;
+  }, []);
+
+  const [firstNameArray, setFirstNameArray] = useState([]);
+  const [firstNameCounter, setFirstNameCounter] = useState(0);
+
+  const [lastNameArray, setLastNameArray] = useState([]);
+  const [lastNameCounter, setLastNameCounter] = useState(0);
+
+  const [firstNameDone, setFirstNameDone] = useState(false);
+
+  useEffect(() => {
+    if (firstNameCounter === firstName.length) {
+      setFirstNameDone(true);
+    } else {
+      setTimeout(() => {
+        setFirstNameArray((prev) => [...prev, firstName[firstNameCounter]]);
+        setFirstNameCounter((prev) =>
+          prev < firstName.length ? prev + 1 : prev
+        );
+      }, Math.floor(Math.random() * 200 + 100));
+      console.log(firstNameArray);
+    }
+  }, [firstNameCounter]);
+
+  useEffect(() => {
+    if (firstNameDone) {
+      setTimeout(() => {
+        if (firstNameDone) {
+        }
+        setLastNameArray((prev) => [...prev, lastName[lastNameCounter]]);
+        setLastNameCounter((prev) =>
+          prev < lastName.length ? prev + 1 : prev
+        );
+      }, Math.floor(Math.random() * 200 + 100));
+      console.log(lastNameArray);
+    }
+  }, [lastNameCounter, firstNameDone]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <AnimatePresence>
+      <MainWrapper layout>
+        <Section
+          style={{
+            backgroundColor: "#292933",
+            backgroundImage: `url(${image.src})`,
+          }}
+          layout
+        >
+          <motion.div
+            style={{
+              display: "flex",
+              alignItems: "flex-end",
+              paddingRight: "20%",
+              justifyContent: "flex-end",
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            {firstNameArray.map((letter, index) => (
+              <motion.span
+                layout
+                key={index}
+                className={myFont.className}
+                initial={{ y: 5, opacity: 0 }}
+                animate={{
+                  y: 0,
+                  opacity: 1,
+                }}
+                transition={{
+                  type: "spring",
+                  stiffness: 1200,
+                  damping: 200,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            {firstNameDone !== true && (
+              <motion.div
+                key={"caret"}
+                style={{ fontWeight: "100" }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+              >
+                |
+              </motion.div>
+            )}
+          </motion.div>
+          <motion.div
+            style={{
+              display: "flex",
+              paddingLeft: "20%",
+              paddingRight: "1%",
+            }}
+          >
+            {lastNameArray.map((letter, index) => (
+              <motion.span
+                layout
+                key={index}
+                className={myFont.className}
+                initial={{ y: 5, opacity: 0 }}
+                animate={{
+                  y: 0,
+                  opacity: 1,
+                }}
+                transition={{
+                  type: "spring",
+                  stiffness: 1200,
+                  damping: 200,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            {firstNameDone && (
+              <motion.div key={"caret"} style={{ fontWeight: "100" }}>
+                |
+              </motion.div>
+            )}
+          </motion.div>
+        </Section>
+        <Section>
+          <h1>Page 1</h1>
+        </Section>
+      </MainWrapper>
+    </AnimatePresence>
+  );
 }
